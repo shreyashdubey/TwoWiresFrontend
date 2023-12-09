52dc5395a1ad237cc6a41d4f3f9116bf6a21647c
shreyashdubey <shreyashlrn@gmail.com>
2023-12-09 10:44:25 +0530

    url changed

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-    baseURL: "https://fastidious-pegasus-4cad61.netlify.app",
+    baseURL: "https://troubled-elk-glasses.cyclic.app",
 
 })
 instance.interceptors.response.use(function(response){return response?.data}, function(error) {
