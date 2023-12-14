0701ad0829ed6f850f40686a3bbdf7af0320c705
faiez22222 <faiezmohd43@gmail.com>
2023-12-14 15:49:48 +0530

    update

--- src/components/AboutStartup.js
+++ src/components/AboutStartup.js
@@ -1,7 +1,7 @@
 // ActiveCompetitions.js
 import React from 'react';
 import { Link } from 'react-router-dom'; // Assuming you're using React Router for navigation
-import { Container, Heading, SimpleGrid, Card, Box, Badge, Text, Image, VStack, HStack, Spacer ,useColorModeValue ,  useBreakpointValue, Center, Flex, Grid , Button} from '@chakra-ui/react';
+import { Container, Heading, SimpleGrid, Card, Box, Badge, Text, Image, VStack, HStack, Spacer ,useColorModeValue ,  useBreakpointValue, Center, Flex, Grid , Button , useMediaQuery} from '@chakra-ui/react';
 import contest1 from './images/contest1.jpeg'
 import contest2  from './images/contest2.jpeg'
 import Layout from './DashBoard.js';
@@ -15,7 +15,9 @@ const AboutStartup = () => {
  const isInView = useInView(ref , {once:true})
  const mainControl = useAnimation()
  const x = useMotionValue(0)
-const opacity = useTransform(x, [-100, 0, 100], [0, 1, 0])
+ const opacity = useTransform(x, [-100, 0, 100], [0, 1, 0])
+ const [isabove425px] =useMediaQuery("(min-width: 426px)");;
+ console.log(isabove425px)
 
  useEffect(()=>{
   console.log(isInView)
@@ -27,10 +29,12 @@ const opacity = useTransform(x, [-100, 0, 100], [0, 1, 0])
   
   return (
        <ParticleBg>
-    <Box minHeight='100vh' bgColor={'blackAlpha.900'} >
-       <Text  as='I' fontSize='9xl' ml='300px' fontWeight='medium'  h='30%' textShadow='4px 4px #1D4044' >Sourced Startup</Text>
+    <Box minHeight='100vh' bgColor={'blackAlpha.900'}  w='full'>
+      <Center>
+       <Text  as='I' fontSize={['4xl' , '5xl' , '5xl' , '7xl', '8xl' , '9xl']}  fontWeight='medium'  h='30%' textShadow='4px 4px #1D4044'  >Sourced Startup</Text>
         {/* Sourced Startup Text */}
-        <VStack  ml='150px' >
+        </Center>
+        <VStack  ml= {['400px' , '20px' , '30px' , '-40px', '30px' , '150px']}  mr= {['400px' , '0px' , '0px' , '0px', '30px' , '150px']}>
          <Box ref={ref}>
         <motion.div
            variants = {{
@@ -43,30 +47,57 @@ const opacity = useTransform(x, [-100, 0, 100], [0, 1, 0])
          
         >
           
-        <HStack  height='100px' mt='100px' ml='100px' spacing='200px' as='I'>
-            <Heading>Vision</Heading>
-            <Text  w='55%'>At SourcedStartup, our vision is rooted in alleviating the common challenges encountered by entrepreneurs throughout the startup journey. We aim to streamline the startup-building process by addressing key pain points and providing comprehensive solutions.</Text>
-        </HStack>
+       {isabove425px ?(
+              <HStack  height={['400px' , '700px' , '250px' , '150px', '200px' , '100px']} mt='100px' ml='100px' spacing={['200px' , '200px' , '20px' , '200px', '200px' , '200px']}  as='I'  >
+              <Heading>Vision</Heading>
+              <Text  w='55%'>At SourcedStartup, our vision is rooted in alleviating the common challenges encountered by entrepreneurs throughout the startup journey. We aim to streamline the startup-building process by addressing key pain points and providing comprehensive solutions.</Text>
+          </HStack>
+       ):(
+          <VStack bgColor={'red.800'} mt='50px' w='100%' justifyContent='center' alignContent='center' >
+            <Heading fontSize='3xl'>Vision</Heading>
+              <Text  w='55%'>At SourcedStartup, our vision is rooted in alleviating the common challenges encountered by entrepreneurs throughout the startup journey. We aim to streamline the startup-building process by addressing key pain points and providing comprehensive solutions.</Text>
+          </VStack>
+       )}
+      {isabove425px ?(
+               <HStack  height={['400px' , '300px' , '400px' , '200px', '200px' , '100px']}  mt= {['400px' , '300px' , '200px' , '30px', '30px' , '30px']} ml='100px' spacing={['200px' , '200px' , '20px' , '95px', '95px' , '95px']}  as='I'  >
+               <Heading  > Key Services</Heading>
+               <Text   w='55%'>Empower your startup journey with SourcedStartup's innovative solutions, offering co-founder matchmaking for like-minded entrepreneurs and contest-based compatibility testing to ensure your team's success from the early stages of your venture</Text>
+           </HStack>
+       ):(
+          <VStack bgColor={'blue.900'} mt='50px'>
+            <Center flexDir={'column'}>
+          <Heading fontSize='3xl'  > Key Services</Heading>
+          <Text   mt='5px'  w='55%'>Empower your startup journey with SourcedStartup's innovative solutions, offering co-founder matchmaking for like-minded entrepreneurs and contest-based compatibility testing to ensure your team's success from the early stages of your venture</Text>
+          </Center>
+          </VStack>
+       )}
+        {isabove425px ?(
+                <HStack  height={['800px' , '800px' , '800px' , '800px', '750px' ,'600px']}  mt= {['400px' , '300px' , '200px' , '100px', '100px' , '30px']} ml='100px'  spacing={['200px' , '200px' , '20px' , '40px', '10px' , '10px']} as='I' >
+                <Heading mb='350px'>Future Goal</Heading>
+                <VStack spacing='30px'  mb='100px' ml='30px' w='70%' as='I' >
+                <Text w='75%' mr='60px'>We are developing an advanced Judge AI that will evaluate proposed solutions from co-founders. This AI will provide objective ratings and create a systematic flow to transform these solutions into actionable plans for building successful startups.</Text>
+                <Text w='75%' mr='60px'>Our platform will introduce a Viability Testing Engine, allowing users to simulate and assess the feasibility of their startup solutions. This tool will provide invaluable insights, enabling entrepreneurs to refine their strategies and make informed decisions.</Text>
+                <Text w='75%' mr='60px' >SourcedStartup envisions fostering an open-source community where individuals can contribute to the step-by-step solutions required for startup development. Collaboration will be key, as community members join forces to build a supportive ecosystem for aspiring entrepreneurs.</Text>
+                <Text w='75%' mr='60px'>To enhance the learning experience, our platform will feature a dynamic feed with posts from fellow creators. Stay updated on industry trends, gain insights from experienced entrepreneurs, and showcase your progress to build a following. This content and community platform will serve as a hub for knowledge exchange and networking within the startup community.</Text>
+                </VStack>
+            </HStack>
+       ):(
+          <VStack bgColor={'green.900'} mt='50px' mb='50px' justifyContent='center' alignContent='center' flexDir={'column'}>
+          <Heading fontSize='3xl' >Future Goal</Heading>
+          <Text w='75%' >We are developing an advanced Judge AI that will evaluate proposed solutions from co-founders. This AI will provide objective ratings and create a systematic flow to transform these solutions into actionable plans for building successful startups.</Text>
+                <Text w='75%' >Our platform will introduce a Viability Testing Engine, allowing users to simulate and assess the feasibility of their startup solutions. This tool will provide invaluable insights, enabling entrepreneurs to refine their strategies and make informed decisions.</Text>
+                <Text w='75%' mr='20px' >SourcedStartup envisions fostering an open-source community where individuals can contribute to the step-by-step solutions required for startup development. Collaboration will be key, as community members join forces to build a supportive ecosystem for aspiring entrepreneurs.</Text>
+                <Text w='75%' mr='20px'>To enhance the learning experience, our platform will feature a dynamic feed with posts from fellow creators. Stay updated on industry trends, gain insights from experienced entrepreneurs, and showcase your progress to build a following. This content and community platform will serve as a hub for knowledge exchange and networking within the startup community.</Text>
 
-        <HStack  height='100px' mt='30px' ml='100px' spacing='95px'  as='I'>
-            <Heading  > Key Services</Heading>
-            <Text   w='55%'>Empower your startup journey with SourcedStartup's innovative solutions, offering co-founder matchmaking for like-minded entrepreneurs and contest-based compatibility testing to ensure your team's success from the early stages of your venture</Text>
-        </HStack>
+          </VStack>
+       )}
         
-        <HStack  height='600px' mt='30px' ml='100px' spacing='0px' as='I'>
-            <Heading mb='350px'>Future Goal</Heading>
-            <VStack spacing='30px'  mb='100px' ml='30px' w='70%' as='I' >
-            <Text w='75%' mr='60px'>We are developing an advanced Judge AI that will evaluate proposed solutions from co-founders. This AI will provide objective ratings and create a systematic flow to transform these solutions into actionable plans for building successful startups.</Text>
-            <Text w='75%' mr='60px'>Our platform will introduce a Viability Testing Engine, allowing users to simulate and assess the feasibility of their startup solutions. This tool will provide invaluable insights, enabling entrepreneurs to refine their strategies and make informed decisions.</Text>
-            <Text w='75%' mr='60px' >SourcedStartup envisions fostering an open-source community where individuals can contribute to the step-by-step solutions required for startup development. Collaboration will be key, as community members join forces to build a supportive ecosystem for aspiring entrepreneurs.</Text>
-            <Text w='75%' mr='60px'>To enhance the learning experience, our platform will feature a dynamic feed with posts from fellow creators. Stay updated on industry trends, gain insights from experienced entrepreneurs, and showcase your progress to build a following. This content and community platform will serve as a hub for knowledge exchange and networking within the startup community.</Text>
-            </VStack>
-        </HStack>
+      
 
        
-
-        <Text mr='200px' mb='100px' >At SourcedStartup, we are committed to empowering entrepreneurs with innovative solutions and a supportive community, paving the way for successful and sustainable startups. Join us on this exciting journey of collaboration, learning, and growth.</Text>
-       
+        <Center  ml={['865px' , '0px' , '0px' , '80px', '70px' ,'60px']}>
+        <Text   mb='100px' >At SourcedStartup, we are committed to empowering entrepreneurs with innovative solutions and a supportive community, paving the way for successful and sustainable startups. Join us on this exciting journey of collaboration, learning, and growth.</Text>
+        </Center>
         </motion.div>
         </Box>
 
