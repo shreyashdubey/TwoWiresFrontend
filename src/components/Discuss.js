c1ef306832d1a5a29de134b10fb7e22ddd8958c4
faiez22222 <faiezmohd43@gmail.com>
2024-01-30 08:47:02 +0530

    update

--- src/components/Discuss.js
+++ src/components/Discuss.js
@@ -1,4 +1,4 @@
-import React, { useState , useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Button,
   Modal,
@@ -14,70 +14,62 @@ import {
   Card,
   HStack,
   Center,
-  Image ,
-  Flex, 
+  Image,
+  Flex,
   Spacer,
 } from '@chakra-ui/react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
-import upvote from './images/upvote.png'
-import downvote from './images/downvote.png'
-import { ADD_CONTEST_PLAN } from '../utils/endpoints';
-import { jwtDecode } from "jwt-decode";
+import upvote from './images/upvote.png';
+import downvote from './images/downvote.png';
+import { ADD_CONTEST_PLAN, ADD_EXECUTION_STEP } from '../utils/endpoints';
+import { jwtDecode } from 'jwt-decode';
 import instance from '../utils/api';
 import {
   useNavigate,
   useLocation,
-  Navigate,
   useParams,
-} from "react-router-dom";
-import { ACCESS_TOKEN } from "../utils/siteConstants";
+} from 'react-router-dom';
+import { ACCESS_TOKEN } from '../utils/siteConstants';
 import { GET_ALL_PLAN } from '../utils/endpoints';
-import { ADD_EXECUTION_STEP } from '../utils/endpoints';
-
 
 const PlanComponent = () => {
-
   const location = useLocation();
   const variable = location.state;
-  console.log('var' , variable)
+  console.log('var', variable);
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [planName, setPlanName] = useState('');
   const [planContent, setPlanContent] = useState('');
   const [plans, setPlans] = useState([]);
   const [initialFetch, setInitialFetch] = useState(false);
-  plans.upvotes = 0;
-  plans.downvotes = 0;
-  const contestId = variable.contestId
-
+  const contestId = variable.contestId;
 
   useEffect(() => {
-    console.log('worked')
+    console.log('worked');
     // Fetch education entries when the component mounts
     const fetchContestPlans = async () => {
       try {
-        console.log('workedwithinfunction')
+        console.log('workedwithinfunction');
         const accessToken = localStorage.getItem(ACCESS_TOKEN);
         const decodedToken = jwtDecode(accessToken);
         const userId = decodedToken.user._id;
         const response = await instance.get(
-          `${ GET_ALL_PLAN}?contestId=${contestId}&page=1&pageSize=10`,
+          `${GET_ALL_PLAN}?contestId=${contestId}&page=1&pageSize=10`,
         );
-        const {plans} = response;
+        const { plans } = response.data; // Assuming plans are present in the response data
         setPlans(plans);
       } catch (error) {
         console.error(
-          "Error occurred while fetching education entries:",
+          'Error occurred while fetching education entries:',
           error,
         );
       }
     };
-   
+
     if (!initialFetch) {
-      console.log('withinif')
+      console.log('withinif');
       fetchContestPlans();
       // Fetch education entries only when initialFetch is false
-     
       setInitialFetch(true); // Set initialFetch to true after the initial fetch
     }
   }, [initialFetch]);
@@ -93,16 +85,14 @@ const PlanComponent = () => {
   const [currentPlanIndex, setCurrentPlanIndex] = useState(null);
   const [currentPlanId, setCurrentPlanId] = useState(null);
 
-  const handleAddExecutionStep = (index , planId) => {
-    console.log('plan' , planId)
+  const handleAddExecutionStep = (index, planId) => {
+    console.log('plan', planId);
     setCurrentPlanIndex(index); // Set the current plan index
-    setCurrentPlanId(planId)
+    setCurrentPlanId(planId);
     onExecutionModalOpen();
   };
 
-
-
-  const handleSaveExecutionStep = async() => {
+  const handleSaveExecutionStep = async () => {
     console.log('maakaxchi', currentPlanIndex);
     if (executionStepContent.trim() === '') {
       // Validation: Don't save if executionStepContent is empty
@@ -113,50 +103,46 @@ const PlanComponent = () => {
       const accessToken = localStorage.getItem(ACCESS_TOKEN);
       const decodedToken = jwtDecode(accessToken);
       const planId = currentPlanId;
-      const executionStep = plans[currentPlanIndex].executionSteps
+      const executionStep = plans[currentPlanIndex].executionSteps;
       const stepNumber = executionStep.length;
-      const stepName = executionStepName
-      const stepDescription = executionStepContent
-     
+      const stepName = executionStepName;
+      const stepDescription = executionStepContent;
 
       const response = await instance.post(
         `${ADD_EXECUTION_STEP}`,
         {
-            planId , stepNumber , stepName   , stepDescription ,
+          planId,
+          stepNumber,
+          stepName,
+          stepDescription,
         },
-        { "Content-Type": "application/json" },
+        { 'Content-Type': 'application/json' },
       );
       const data = response;
       const { success, message } = data;
       if (success) {
         // Handle success
-        console.log("Data successfully posted to the backend");
+        console.log('Data successfully posted to the backend');
 
         // Clear form data and reset state after successful post
-       
       } else {
         // Handle error
-        console.error("Failed to post data to the backend");
+        console.error('Failed to post data to the backend');
       }
     } catch (error) {
-      console.error("Error occurred while posting data:", error);
+      console.error('Error occurred while posting data:', error);
     }
-
-    // Associate execution step with the current plan using currentPlanIndex
-   
-  
 
     // Clear input field
     setExecutionStepContent('');
-    setExecutionStepName('')
-    
+    setExecutionStepName('');
 
     // Close the modal
     onExecutionModalClose();
     setInitialFetch(false);
   };
 
-  const handleSave = async() => {
+  const handleSave = async () => {
     if (planName.trim() === '' || planContent.trim() === '') {
       // Validation: Don't save if either planName or planContent is empty
       return;
@@ -166,39 +152,37 @@ const PlanComponent = () => {
       const accessToken = localStorage.getItem(ACCESS_TOKEN);
       const decodedToken = jwtDecode(accessToken);
       const userId = decodedToken.user._id;
-     
-     const planDescription = planContent;
+
+      const planDescription = planContent;
 
       const response = await instance.post(
         `${ADD_CONTEST_PLAN}?contestId=${contestId}&userId=${userId}`,
         {
-          planName , planDescription
+          planName,
+          planDescription,
         },
-        { "Content-Type": "application/json" },
+        { 'Content-Type': 'application/json' },
       );
       const data = response;
       const { success, message } = data;
       if (success) {
         // Handle success
-        console.log("Data successfully posted to the backend");
+        console.log('Data successfully posted to the backend');
 
         // Clear form data and reset state after successful post
-       
       } else {
         // Handle error
-        console.error("Failed to post data to the backend");
+        console.error('Failed to post data to the backend');
       }
     } catch (error) {
-      console.error("Error occurred while posting data:", error);
+      console.error('Error occurred while posting data:', error);
     }
 
     // Save the plan to the state with an empty executionSteps array
-   
 
     // Reset the input fields
     setPlanName('');
     setPlanContent('');
-   
 
     // Close the modal
     onClose();
@@ -208,17 +192,16 @@ const PlanComponent = () => {
   const handleUpvote = (index) => {
     const updatedPlans = [...plans];
     updatedPlans[index].upvotes = (updatedPlans[index].upvotes || 0) + 1;
-    if(!updatedPlans[index].downvotes) updatedPlans[index].downvotes = 0
+    if (!updatedPlans[index].downvotes) updatedPlans[index].downvotes = 0;
     setPlans(updatedPlans);
   };
 
-  const handleDownvote = (index) => {
-    const updatedPlans = [...plans];
-    updatedPlans[index].downvotes = (updatedPlans[index].downvotes || 0) -1;
-    if(!updatedPlans[index].upvotes) updatedPlans[index].upvotes = 0
-    setPlans(updatedPlans);
-  };
-
+  // const handleDownvote = (index) => {
+  //   const updatedPlans = [...plans]
+  //   updatedPlans[index].downvotes = (updatedPlans[index].downvotes || 0) - 1
+  //   if (!updatedPlans[index].upvotes) updatedPlans[index].upvotes = 0
+  //   setPlans(updatedPlans)
+  // }
 
   return (
     <Box p={4}>
@@ -234,11 +217,17 @@ const PlanComponent = () => {
           <ModalBody>
             <Box mb={4}>
               <Text mb={2}>Plan Name:</Text>
-              <ReactQuill value={planName} onChange={(value) => setPlanName(value)} />
+              <ReactQuill
+                value={planName}
+                onChange={(value) => setPlanName(value)}
+              />
             </Box>
             <Box>
               <Text mb={2}>Plan Content:</Text>
-              <ReactQuill value={planContent} onChange={(value) => setPlanContent(value)} />
+              <ReactQuill
+                value={planContent}
+                onChange={(value) => setPlanContent(value)}
+              />
             </Box>
           </ModalBody>
           <ModalFooter>
@@ -252,37 +241,46 @@ const PlanComponent = () => {
 
       {plans.map((plan, index) => (
         <Card key={index} my={4} p={4}>
-              <HStack>
-              <HStack>
+          <HStack>
+            <HStack>
               <Text fontSize="md" fontWeight="bold">
                 Plan Name:
               </Text>
               <div dangerouslySetInnerHTML={{ __html: plan.planName }} />
-              </HStack>
-              <Spacer/>
-              <Flex       w="10%">
-                  <Button variant="solid" size="sm" onClick={() => handleUpvote(index)}>
-                    <Image src= {upvote} alt="Upvote" boxSize="20px" />
-                  </Button>
-                  <Button variant="solid" size="sm" onClick={() => handleDownvote(index)}>
-                    <Image src= {downvote}  alt="Downvote" boxSize="20px" />
-                  </Button>
-                  <Text ml='10px' >
-                  {(plan.upvotes+plan.downvotes>=0) ? (
-                    <>
-                      {plan.upvotes+plan.downvotes}
-                    </>
-                  ):(
-                      <>
-                        0
-                      </>
-                  ) 
-                  }
-                  </Text>
-                </Flex>
+            </HStack>
+            <Spacer />
+            <Flex w="10%">
+              <Button
+                variant="solid"
+                size="sm"
+                onClick={() => handleUpvote(index)}
+              >
+                <Image src={upvote} alt="Upvote" boxSize="20px" />
+              </Button>
+              <Button
+                variant="solid"
+                size="sm"
+                onClick={() => handleDownvote(index)}
+              >
+                <Image src={downvote} alt="Downvote" boxSize="20px" />
+              </Button>
+              <Text ml="10px">
+                {plan.upvotes + plan.downvotes >= 0 ? (
+                  <>
+                    {plan.upvotes + plan.downvotes}
+                  </>
+                ) : (
+                  <>
+                    0
+                  </>
+                )}
+              </Text>
+            </Flex>
           </HStack>
           <HStack>
-          <Text fontSize="md" fontWeight="bold" >Plan Content:</Text>
+            <Text fontSize="md" fontWeight="bold">
+              Plan Content:
+            </Text>
             <div dangerouslySetInnerHTML={{ __html: plan.planDescription }} />
           </HStack>
           {plan.executionSteps.map((step, stepIndex) => (
@@ -298,24 +296,27 @@ const PlanComponent = () => {
             </Card>
           ))}
           <Center>
-          <Button
-            onClick={() => handleAddExecutionStep(index , plan._id )}
-            colorScheme="teal"
-            size="sm"
-            mt={2}
-            w='10%'
-          >
-            Add Execution Step
-          </Button>
+            <Button
+              onClick={() => handleAddExecutionStep(index, plan._id)}
+              colorScheme="teal"
+              size="sm"
+              mt={2}
+              w="10%"
+            >
+              Add Execution Step
+            </Button>
           </Center>
-          <Modal isOpen={isExecutionModalOpen} onClose={onExecutionModalClose}>
+          <Modal
+            isOpen={isExecutionModalOpen}
+            onClose={onExecutionModalClose}
+          >
             <ModalOverlay />
             <ModalContent>
               <ModalHeader>Add Execution Step</ModalHeader>
               <ModalCloseButton />
               <ModalBody>
                 <Box mb={4}>
-                <Text mb={2}>Execution Step name:</Text>
+                  <Text mb={2}>Execution Step name:</Text>
                   <ReactQuill
                     value={executionStepName}
                     onChange={(value) => setExecutionStepName(value)}
@@ -339,7 +340,6 @@ const PlanComponent = () => {
               </ModalFooter>
             </ModalContent>
           </Modal>
-          
         </Card>
       ))}
     </Box>
